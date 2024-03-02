a27a65da4a54a999240bef01476ebb1c93c84550
Nisal Don <nisal.don@convertdigital.com.au>
2024-03-02 23:29:45 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = { pkgs, ... }: {
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    darwinConfigurations."Nisals-MacBook-Pro-2" = nix-darwin.lib.darwinSystem {
+      # Set Git commit hash for darwin-version.
+      modules = [ 
+        configuration
+        ./configuration.nix 
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          users.users.nisaldon = {
+            name = "nisaldon";
+            home = "/Users/nisaldon";
+          };
+          home-manager.users.nisaldon = import ./home.nix;
+# Optionally, use home-manager.extraSpecialArgs to pass
+# arguments to home.nix
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."Nisals-MacBook-Pro-2".pkgs;
+  };
+}
