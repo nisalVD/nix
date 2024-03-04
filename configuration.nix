4b5fd5dca2dfd6e788d942215999ad7eb955e7c8
Nisal Don <nisal.don@convertdigital.com.au>
2024-03-05 02:52:30 +1100

    dont remove this

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,8 @@
       services.nix-daemon.enable = true;
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
+      # need to enable this here or it will break
+      programs.zsh.enable = true;  # default shell on catalina
       system.stateVersion = 4;
   }
 
